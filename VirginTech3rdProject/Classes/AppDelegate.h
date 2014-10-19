744138be4b74f68a0cab0aaf498ee381c000b610
OOTANI,Kenji <ootani@virgintech.co.jp>
2014-10-19 11:02:58 +0900

    Initial Commit

---
+++ VirginTech3rdProject/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  VirginTech3rdProject
+//
+//  Created by VirginTech LLC. on 2014/10/19.
+//  Copyright VirginTech LLC. 2014年. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ VirginTech3rdProject/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  VirginTech3rdProject
+//
+//  Created by VirginTech LLC. on 2014/10/19.
+//  Copyright VirginTech LLC. 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
